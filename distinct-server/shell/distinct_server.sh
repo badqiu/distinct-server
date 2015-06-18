3aba724299c647b82063c7f03dc0ef0a2b176f22
badqiu <badqiu@gmail.com>
2015-06-18 16:11:01 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- distinct-server/shell/distinct_server.sh
+++ distinct-server/shell/distinct_server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 source /etc/profile
 
+shell_dir=$(dirname $_)
+
 ############  example ##################
 #distinct_server.sh -DdataDir=/data/bloomfilter_db -Dport=
 ########################################
 params=$@
 
-cmd="java  -cp ../target/distinct-server.jar:$ANA_HOME $params com.github.distinct_server.Server" 
+cmd="java  -cp ${shell_dir}/../target/distinct-server.jar:$ANA_HOME $params com.github.distinct_server.Server" 
 echo '-----------------------------------------------'
 echo $cmd
 echo '-----------------------------------------------'
